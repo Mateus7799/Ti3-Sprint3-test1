035857c590aa23495ca2a165f41cae13b2fce6c3
Mateus7799 <136096790+Mateus7799@users.noreply.github.com>
2025-10-15 17:23:33 -0300

    Updated servicos-tabs.ts

--- Codigo/Front/Vaztech/src/app/pages/hub/servicos-tabs/servicos-tabs.ts
+++ Codigo/Front/Vaztech/src/app/pages/hub/servicos-tabs/servicos-tabs.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servicos-tabs',
+  standalone: true,
   imports: [],
   templateUrl: './servicos-tabs.html',
   styleUrl: './servicos-tabs.css',
